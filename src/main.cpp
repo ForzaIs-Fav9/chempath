91d962845ab1466317e3f1578b095bf8e406439f
Harshith Vankela <89690651+harshithvankela@users.noreply.github.com>
2026-07-25 17:10:26 +0530

    refactor: use graph module for graph construction and pathfinding

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,24 @@ int main()
     vector<vector<Reaction>> graph(n);
 
     addReaction(graph, 0, 1, 29.5);
-    addReaction(graph, 0, 2, 5);
+    addReaction(graph, 0, 2, 5.0);
 
     addReaction(graph, 1, 2, 78.9);
 
     addReaction(graph, 2, 3, 84.6);
-    addReaction(graph, 2, 3, 9);
+    addReaction(graph, 2, 3, 9.0);
 
     addReaction(graph, 3, 0, 97.0);
 
+    cout << "Reaction Network\n" << endl;
+
     printGraph(graph);
+
+    vector<double> dist = dijkstra(graph, 0);
+
+    cout << endl;
+
+    printDistances(dist);
 
     return 0;
 }
